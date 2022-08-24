f715392cbc138cc9f25b01f9c002696864828bd1
kosmos1308 <pasha13kosmos@gmail.com>
2022-08-24 23:19:32 +0300

    Create Todo List

---
+++ ToDoListApp/Views/ListView.swift
@@ -0,0 +1,46 @@
+//
+//  ListView.swift
+//  ToDoListApp
+//
+//  Created by pavel on 24.08.22.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    
+    @State var items: [String] = [
+        "This is the first item",
+        "This is the second item",
+        "This is the third item"
+    ]
+    
+    var body: some View {
+        List {
+            ForEach(items, id: \.self) { item in
+                ListRowView(title: item)
+            }
+        }
+        .listStyle(PlainListStyle())
+        .navigationTitle("Todo List 📝")
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                EditButton()
+            }
+            ToolbarItem(placement: .navigationBarTrailing) {
+                NavigationLink("Add") {
+                    EmptyView()
+                }
+            }
+        }
+
+    }
+}
+
+struct ListView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            ListView()
+        }
+    }
+}
