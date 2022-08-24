1c4c7589da9f6fa88ca9e1fa5c6a23d31c8c46fb
kosmos1308 <pasha13kosmos@gmail.com>
2022-08-24 22:50:34 +0300

    Initial commit

---
+++ ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by pavel on 24.08.22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
